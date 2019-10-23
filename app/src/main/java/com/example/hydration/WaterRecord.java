e48d47a09300a13df527b153aa0f4cdbfda767c9
JamesGardner1 <jamesacgardner@yahoo.com>
2019-10-23 16:43:02 -0500

    Hydration

---
+++ app/src/main/java/com/example/hydration/WaterRecord.java
@@ -0,0 +1,39 @@
+package com.example.hydration;
+
+import androidx.annotation.NonNull;
+import androidx.room.PrimaryKey;
+
+public class WaterRecord {
+
+    @NonNull
+    @PrimaryKey
+    private String day;
+
+    private int glasses;
+
+    public WaterRecord(@NonNull String day, int glasses) {
+        this.day = day;
+        this.glasses = glasses;
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    public void setDay(String day) {
+        this.day = day;
+    }
+
+    public int getGlasses() {
+        return glasses;
+    }
+
+    public void setGlasses(int glasses) {
+        this.glasses = glasses;
+    }
+
+    @Override
+    public String toString() {
+        return "WaterRecord{" + "glasses=" + glasses + ", days=" + day + '}';
+    }
+}
